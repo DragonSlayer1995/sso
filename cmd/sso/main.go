e66063cdc66b914534b95632aafc1d6b1c21e1f9
pstrakhov <pstrakhov@ozon.ru>
2024-07-02 11:16:40 +0300

    big init - config, logger and start grpc

---
+++ cmd/sso/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"sso/interanl/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application",
+		slog.String("env", cfg.Env),
+		slog.Any("cfg", cfg),
+		slog.Int("port", cfg.GRPC.Port),
+	)
+
+	log.Debug("debug message")
+	log.Warn("warning message")
+	log.Error("error message")
+
+	//TODO: инициализировать объект конфига
+
+	//TODO: инициализировать логгер - slog
+
+	//TODO: инициализация приложения (app)
+
+	//TODO: запустить gRPC-сервер приложения
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+
+	return log
+}
